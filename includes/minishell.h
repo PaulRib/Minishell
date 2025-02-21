17d1e95d67d6e730e97430658dc81ccc4dbfbaab
Paul Ribolzi <pribolzi@k1r1p12.42mulhouse.fr>
2025-02-21 16:12:47 +0100

    Libft et Makefile

---
+++ includes/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pribolzi <pribolzi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 15:03:45 by pribolzi          #+#    #+#             */
+/*   Updated: 2025/02/21 15:14:07 by pribolzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+#endif
