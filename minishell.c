61a975f2a4cb85b399e7d80109f93cc5761610b3
Paul Ribolzi <pribolzi@k1r1p10.42mulhouse.fr>
2025-02-26 16:07:03 +0100

    builtin

--- minishell.c
+++ minishell.c
@@ -6,11 +6,30 @@
 /*   By: pribolzi <pribolzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 15:03:41 by pribolzi          #+#    #+#             */
-/*   Updated: 2025/02/26 16:05:02 by pribolzi         ###   ########.fr       */
+/*   Updated: 2025/02/26 16:06:54 by pribolzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+
+int	is_builtin(char *built)
+{
+	if (!ft_strcmp(built, "echo"))
+		return (1);
+	if (!ft_strcmp(built, "cd"))
+		return (1);
+	if (!ft_strcmp(built, "pwd"))
+		return (1);
+	if (!ft_strcmp(built, "export"))
+		return (1);
+	if (!ft_strcmp(built, "unset"))
+		return (1);
+	if (!ft_strcmp(built, "env"))
+		return (1);
+	if (!ft_strcmp(built, "exit"))
+		return (1);
+	return (0);
+}
 
 int	main(void)
 {
@@ -24,7 +43,6 @@ int	main(void)
 			ft_printf("\033[1;33mexit\033[0m\n");
 			break ;
 		}
-		if ()
 		free(line);
 	}
 	return (0);
