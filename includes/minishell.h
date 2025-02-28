1def3e7ad88c2340c53325bc1cbb8dc3144bb066
Mehdi El Warrad <meel-war@k1r1p13.42mulhouse.fr>
2025-02-28 15:56:02 +0100

    includes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: meel-war <meel-war@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 15:03:45 by pribolzi          #+#    #+#             */
-/*   Updated: 2025/02/28 14:21:30 by meel-war         ###   ########.fr       */
+/*   Updated: 2025/02/28 15:54:58 by meel-war         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 
-int	g_exit_status;
+// int	g_exit_status;
 
 typedef struct s_data
 {
@@ -28,26 +28,29 @@ typedef struct s_data
 	char **new_env;
 }			t_data;
 
-typedef struct s_list
-{
-	char	*str;
-	t_data	*next;
-	t_data	*prev;
-	t_token	*token;
-}					t_list;
+char **copy_env(char **env);
+char **build_env(void);
 
-typedef struct s_token
-{
-	int				token;
-	char			*str;
-	struct s_token	*next;
-}				t_token;
+// typedef struct s_list
+// {
+// 	char	*str;
+// 	t_data	*next;
+// 	t_data	*prev;
+// 	t_token	*token;
+// }					t_list;
 
-typedef struct s_pipex
-{
-	int	p_fd[2];
-	int	fd_in;
-	int	fd_out;
-}				t_pipex;
+// typedef struct s_token
+// {
+// 	int				token;
+// 	char			*str;
+// 	struct s_token	*next;
+// }				t_token;
+
+// typedef struct s_pipex
+// {
+// 	int	p_fd[2];
+// 	int	fd_in;
+// 	int	fd_out;
+// }				t_pipex;
 
 #endif
