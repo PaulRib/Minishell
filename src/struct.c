1aa927053d77187893bf75c7d6239a69662b1a63
Paul Ribolzi <pribolzi@k1r1p15.42mulhouse.fr>
2025-03-12 18:30:42 +0100

    lol

--- src/struct.c
+++ src/struct.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meel-war <meel-war@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pribolzi <pribolzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:00:15 by pribolzi          #+#    #+#             */
-/*   Updated: 2025/03/12 17:54:47 by meel-war         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:29:42 by pribolzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,18 @@ void	new_node_token(t_token *ptr, bool place)
 	new = malloc(sizeof(t_token));
 	if (!new)
 		return ;
-	if(place == true)
+	if (place == true)
 	{
 		while (ptr->next)
 			ptr = ptr->next;
 		ptr->next = new;
 		new->prev = ptr;
+		new->next = NULL;
 	}
-	new->next = NULL;
+	else if (bool == false)
+	{
+		new->next = ptr->next;
+		ptr->next = new->previous;
+		ptr->next = new;
+	}
 }
