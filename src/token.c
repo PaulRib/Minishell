749248507e604002a5467f6b8e3c22689dffe289
Mehdi El Warrad <meel-war@k1r1p10.42mulhouse.fr>
2025-03-13 15:46:08 +0100

    builtin

--- src/token.c
+++ src/token.c
@@ -6,7 +6,7 @@
 /*   By: meel-war <meel-war@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 15:14:24 by pribolzi          #+#    #+#             */
-/*   Updated: 2025/03/13 14:57:11 by meel-war         ###   ########.fr       */
+/*   Updated: 2025/03/13 15:45:49 by meel-war         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,11 +91,12 @@ void ft_split_word(t_token *token)
 {
 	char		*dest;
 	int		i;
+	int start_j = j;
 
 	if (!token->str)
 		return (NULL);
 	i = 0;
-	dest = malloc(sizeof(char) * ft_strlen(token->str, j) + 1);
+	dest = malloc(sizeof(char) * ft_strlen_token(token->str, j) + 1);
 	if (!dest)
 		return (NULL);
 	while (token->str[j] && token->str[j] != ' ' && token->str[j] != '\t')
@@ -105,10 +106,37 @@ void ft_split_word(t_token *token)
 		j++;
 	}
 	dest[i] = '\0';
+	ft_remove_token_word(token, start_j);
 	return (dest);
 }
+static void ft_remove_token_word(t_token *token, int j)
+{
+	int word_len = 0;
+	int i = j;
+	int k = 0;
+	int total_len = ft_strlen(token->str, 0);
+	char *new_str;
+	
+		while(token->str[i] && (token->str[i] != ' ' || token->str[i] != '\t'))
+		{
+			word_len++;
+			i++;
+		}
+		while(token->str[i] == ' ' || token->str[i] == '\t')
+			i++;
+		new_str = malloc(sizeof(char) * total_len - word_len + 1);
+		if(!new_str)
+			return(NULL);
+		while(k < j)
+			new_str[k++] = token->str[k++];
+		while(token->str[i])
+			new_str[k++] = token->str[i++];
+		new_str[k] = '\0';
+	free(token->str);
+	token->str = new_str;
+}
 
-static int	ft_strlen(t_token *token, int j)
+static int	ft_strlen_token(t_token *token, int j)
 {
 	int	i;
 
