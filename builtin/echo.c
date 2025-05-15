6feb8c34f2b8566b82812f3cbd542c2b331b4e12
meel-war <meelwar.42@gmail.com>
2025-05-15 16:37:26 +0200

    Update echo.c

--- builtin/echo.c
+++ builtin/echo.c
@@ -14,163 +14,75 @@
 
 static void	new_node_echo(t_token *current, int start)
 {
-	t_token	*new_token;
-	char	*echo_str;
+	t_token	*new;
+	char	*echo;
 
-	new_token = malloc(sizeof(t_token));
-	if (!new_token)
-		return ;
-	new_token->str = ft_substr(current->str, start, 
-			ft_strlen(current->str) - start);
-	echo_str = ft_substr(current->str, 0, start);
+	new = malloc(sizeof(t_token));
+	new->str = ft_substr(current->str, start, ft_strlen(current->str) - start);
+	echo = ft_substr(current->str, 0, start - 1);
 	free(current->str);
-	current->str = echo_str;
-	new_token->type = WORD;
-	new_token->next = current->next;
-	if (current->next)
-		current->next->prev = new_token;
-	new_token->prev = current;
-	current->next = new_token;
+	new->type = WORD;
+	current->str = echo;
+	new->next = current->next;
+	current->next = new;
+	current->next->prev = new;
+	new->prev = current;
 }
 
-static int	handle_n_option_in_str(const char *str, int *i, int *n_opt_count)
+int	ft_count(t_token *current)
 {
-	if (ft_strncmp(&str[*i], "-n", 2) == 0)
-	{
-		*i += 2;
-		(*n_opt_count)++;
-		while (str[*i] == 'n')
-			(*i)++;
-		return (1);
-	}
-	return (0);
-}
+	int		i;
+	int		n;
+	t_token	*tmp;
 
-static int	process_options_in_token(t_token *echo_cmd_token)
-{
-	int	i;
-	int	n_options_count;
-
-	i = 4;
-	n_options_count = 0;
-	while (echo_cmd_token->str && echo_cmd_token->str[i])
+	tmp = current;
+	i = 0;
+	n = 0;
+	while (tmp->str[i])
 	{
-		while (echo_cmd_token->str[i] == ' ')
+		while (i < 4)
 			i++;
-		if (echo_cmd_token->str[i] == '\0')
-			break ;
-		if (!handle_n_option_in_str(echo_cmd_token->str, &i, &n_options_count))
+		if (ft_strncmp(&tmp->str[i], "-n", 2) == 0)
 		{
-			if (echo_cmd_token->str[i] != '\0')
-				new_node_echo(echo_cmd_token, i);
-			break;
+			i += 2;
+			n++;
 		}
+		else if (tmp->str[i] != '\0' && tmp->str[i] != ' ')
+		{
+			new_node_echo(tmp, i);
+			return (n);
+		}
+		i++;
 	}
-	return (n_options_count);
-}
-
-static int	is_valid_n_option_token(const char *s)
-{
-	int	j;
-
-	if (!s || s[0] != '-' || s[1] != 'n')
-		return (0);
-	j = 2;
-	while (s[j] == 'n')
-		j++;
-	if (s[j] == '\0')
-		return (1);
-	return (0);
-}
-
-static int	check_and_count_n_options(t_token *token_ptr, t_token **first_arg)
-{
-	int	n_opt;
-
-	n_opt = process_options_in_token(token_ptr);
-	*first_arg = token_ptr->next;
-	while (*first_arg && (*first_arg)->str && 
-			is_valid_n_option_token((*first_arg)->str) &&
-			((*first_arg)->type == WORD || (*first_arg)->type == S_QUOTE || 
-			(*first_arg)->type == D_QUOTE))
-	{
-		n_opt++;
-		*first_arg = (*first_arg)->next;
-	}
-	return (n_opt);
-}
-
-static void	print_special_case(t_token *arg1, t_token *arg2, t_token *arg3)
-{
-	ft_putstr_fd(arg1->str, 1);
-	ft_putstr_fd(arg2->str, 1);
-	ft_putstr_fd(arg3->str, 1);
-}
-
-static int	is_matching_quote_arg(const char *str, char quote_char)
-{
-	if (str && str[0] == quote_char && str[1] == '\0')
-		return (1);
-	return (0);
-}
-
-static int	is_special_echo_case(t_token *current_arg)
-{
-	char	first_quote_char;
-
-	if (!current_arg || !current_arg->next || !current_arg->next->next)
-		return (0);
-	if (is_matching_quote_arg(current_arg->str, '"'))
-		first_quote_char = '"';
-	else if (is_matching_quote_arg(current_arg->str, '\''))
-		first_quote_char = '\'';
-	else
-		return (0);
-	if (is_matching_quote_arg(current_arg->next->next->str, first_quote_char))
-		return (1);
-	return (0);
-}
-
-static void	print_normal_echo_args(t_token *current_arg)
-{
-	int	first_arg_printed;
-
-	first_arg_printed = 0;
-	while (current_arg && (current_arg->type == WORD ||
-			current_arg->type == S_QUOTE || current_arg->type == D_QUOTE))
-	{
-		if (first_arg_printed)
-			write(1, " ", 1);
-		ft_putstr_fd(current_arg->str, 1);
-		first_arg_printed = 1;
-		current_arg = current_arg->next;
-	}
+	return (n);
 }
 
 int	ft_echo(t_token *token_ptr)
 {
-	t_token	*current_arg;
-	int		n_option_present;
+	t_token	*current;
+	int		n_param;
 
-	n_option_present = check_and_count_n_options(token_ptr, &current_arg);
-	if (is_special_echo_case(current_arg))
+	n_param = 0;
+	n_param = ft_count(token_ptr);
+	current = token_ptr->next;
+	while (current && (current->type == WORD || current->type == S_QUOTE
+			|| current->type == D_QUOTE))
 	{
-		print_special_case(current_arg, current_arg->next, 
-			current_arg->next->next);
+		ft_putstr_fd(current->str, 1);
+		if (current->next && (current->next->type == WORD
+				|| current->next->type == S_QUOTE
+				|| current->next->type == D_QUOTE))
+			write(1, " ", 1);
+		current = current->next;
 	}
-	else
-	{
-		print_normal_echo_args(current_arg);
-	}
-	if (n_option_present == 0)
+	if (n_param == 0)
 		write(1, "\n", 1);
 	return (0);
 }
 
 int	check_echo(t_token *token_ptr)
 {
-	if (!token_ptr || !token_ptr->str || 
-		ft_strncmp(token_ptr->str, "echo", 4) != 0)
+	if (ft_strncmp(token_ptr->str, "echo", 5) != 0)
 		return (-1);
 	return (ft_echo(token_ptr));
 }
