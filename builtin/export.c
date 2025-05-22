2181b3ef5adbb337447f293cdfd9ed35687a6566
Mehdi El Warrad <meel-war@k1r1p12.42mulhouse.fr>
2025-05-22 21:07:56 +0200

    export += ajoute, need export VAR sans rien

--- builtin/export.c
+++ builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: meel-war <meel-war@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:38:15 by meel-war          #+#    #+#             */
-/*   Updated: 2025/05/16 17:25:19 by meel-war         ###   ########.fr       */
+/*   Updated: 2025/05/22 20:56:24 by meel-war         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static int	is_valid_identifier(char *var)
 	i = 1;
 	if (!var || !*var)
 		return (0);
-	while (var[len] && var[len] != '=')
+	while (var[len] && var[len] != '=' && var[len] != '+')
 		len++;
 	if (len == 0 || (!ft_isalpha(var[0]) && var[0] != '_'))
 		return (invalid_identifier(var));
@@ -40,6 +40,40 @@ static int	is_valid_identifier(char *var)
 		i++;
 	}
 	return (-1);
+}
+static int	handle_plus_equal(t_data *data, char *var, char *equal_sign,
+		char **var_name, char **var_value)
+{
+	char	*plus_sign;
+	char	*old_value;
+	char	*new_value;
+
+	plus_sign = ft_strchr(var, '+');
+	if (!plus_sign || plus_sign[1] != '=' || plus_sign >= equal_sign)
+		return (0);
+	*var_name = ft_substr(var, 0, plus_sign - var);
+	if(!*var_name)
+		return(0); //free_all
+	*var_value = ft_strdup(equal_sign + 1);
+	if(!*var_value)
+	{
+		free(*var_name);
+		//free_all
+	}
+	old_value = ft_get_env(data->new_env, *var_name);
+	if (old_value)
+	{
+		new_value = ft_strjoin(old_value, *var_value);
+		if(!new_value)
+		{
+			free(*var_name);
+			free(*var_value);
+			//free_all;
+		}
+		free(*var_value);
+		*var_value = new_value;
+	}
+	return (1);
 }
 
 static int	export_var(t_data *data, char *var)
@@ -53,8 +87,18 @@ static int	export_var(t_data *data, char *var)
 	equal_sign = ft_strchr(var, '=');
 	if (!equal_sign)
 		return (0);
-	var_name = ft_substr(var, 0, equal_sign - var);
-	var_value = ft_strdup(equal_sign + 1);
+	if (!handle_plus_equal(data, var, equal_sign, &var_name, &var_value))
+	{
+		var_name = ft_substr(var, 0, equal_sign - var);
+		if (!var_name)
+			return (-1); //free_all peut etre
+		var_value = ft_strdup(equal_sign + 1);
+		if(!var_value)
+		{
+			free(var_name);
+			return(-1); //free_all
+		}
+	}
 	update_env_var(data, var_name, var_value);
 	free(var_name);
 	free(var_value);
@@ -72,12 +116,15 @@ int	check_export(t_shell *shell, t_token *token_ptr)
 		return (0);
 	}
 	token_ptr = token_ptr->next;
-	while (token_ptr && (token_ptr->type == WORD || token_ptr->type == S_QUOTE || token_ptr->type == D_QUOTE))
+	while (token_ptr && (token_ptr->type == WORD || token_ptr->type == S_QUOTE
+			|| token_ptr->type == D_QUOTE))
 	{
-		if(token_ptr->next)
+		if (token_ptr->next)
 		{
-			if(token_ptr->next->type == S_QUOTE || token_ptr->next->type == D_QUOTE)
-				token_ptr->str = ft_strjoin(token_ptr->str, token_ptr->next->str);
+			if (token_ptr->next->type == S_QUOTE
+				|| token_ptr->next->type == D_QUOTE)
+				token_ptr->str = ft_strjoin(token_ptr->str,
+						token_ptr->next->str);
 		}
 		if (!export_var(shell->data, token_ptr->str))
 			return (-1);
