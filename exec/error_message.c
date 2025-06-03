396e48fc7b7e09e97a9356435b18cf627fbf2c72
Paul <pribolzi@k2r4p4.42mulhouse.fr>
2025-06-03 17:59:48 +0200

    pq ca marche tjr pas ???????????????

--- exec/error_message.c
+++ exec/error_message.c
@@ -6,17 +6,18 @@
 /*   By: pribolzi <pribolzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 18:08:27 by meel-war          #+#    #+#             */
-/*   Updated: 2025/06/03 15:29:34 by pribolzi         ###   ########.fr       */
+/*   Updated: 2025/06/03 17:25:26 by pribolzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	print_access_error(t_token *current)
+int	print_access_error(t_token *current)
 {
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(current->str, 2);
 	ft_putstr_fd(": Permission denied\n", 2);
+	return (1);
 }
 
 void	print_directory_error(t_token *current)
@@ -26,7 +27,7 @@ void	print_directory_error(t_token *current)
 	ft_putstr_fd(": Is a directory\n", 2);
 }
 
-int	verify_access_fd(t_token *current)
+int	verify_access_fd(t_token *current, int type)
 {
 	struct stat	info;
 
@@ -36,11 +37,12 @@ int	verify_access_fd(t_token *current)
 			print_directory_error(current);
 			return (1);
 		}
-	if (access(current->str, R_OK) == -1)
-	{
-		print_access_error(current);
-		return (1);
-	}
+	if (type == 0)
+		if (access(current->str, R_OK) == -1)
+			return (print_access_error(current));
+	if (type == 1)
+		if (access(current->str, W_OK) == -1)
+			return (print_access_error(current));
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(current->str, 2);
 	ft_putstr_fd(": No such file or directory\n", 2);
